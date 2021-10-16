4aedfa8f91f5053d07abaeae561f6d0f156d2a8a
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-10-16 17:27:52 +0700

    Add factory for class room

---
+++ src/model/room/RoomFactory.java
@@ -0,0 +1,23 @@
+package model.room;
+
+import java.util.Scanner;
+
+public class RoomFactory {
+    private RoomFactory() {}
+    public static final Room getRoom(RoomType roomType) {
+        System.out.println("Enter the stage code of room:");
+        Scanner inputCode = new Scanner(System.in);
+        String code = inputCode.nextLine();
+       switch (roomType) {
+           case DELUXERROOM:
+               return new RoomDeluxe(code);
+           case VIPROOM:
+               return new RoomVip(code);
+           case KINGROOM:
+               return new RoomKing(code);
+           default:
+               throw new IllegalArgumentException("This type of room is unsupported");
+       }
+    }
+
+}
