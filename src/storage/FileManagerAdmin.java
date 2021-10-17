1f94e4cc2c99ecea90b77896ab92e653d2b66a26
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-10-17 17:29:44 +0700

    Add filemanager Admin

--- src/storage/FileManagerAdmin.java
+++ src/storage/FileManagerAdmin.java
@@ -1,46 +1,44 @@
 package storage;
 
-import model.Bill;
+import login.Admin;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileManagerBill extends BaseFileManager<Bill> {
-    private static FileManagerBill fileManagerBill;
-private FileManagerBill() {}
-    public static FileManagerBill getInstance() {
-    if (fileManagerBill == null) {
-        fileManagerBill = new FileManagerBill();
+public class FileManagerAdmin extends BaseFileManager<Admin> {
+    private static FileManagerAdmin fileManagerAdmin;
+    private FileManagerAdmin() {}
+    public static FileManagerAdmin getInstance() {
+        if (fileManagerAdmin == null) {
+            fileManagerAdmin = new FileManagerAdmin();
+        }
+        return fileManagerAdmin;
     }
-    return fileManagerBill;
-    }
-    @Override
-    public void writeList(List<Bill> list) {
 
+    @Override
+    void writeList(List<Admin> list) {
         if (list == null) {
             list = new ArrayList<>();
         }
-        File file = new File("listBill.txt");
+        File file =  new File("listAdmin.txt");
         try {
             FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
+           ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(list);
             oos.close();
             fos.close();
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
-    public List<Bill> readList()  {
-        List<Bill> billList = new ArrayList<>();
-        File file = new File("listBill.txt");
+    List<Admin> readList() {
+        List<Admin> adminList = new ArrayList<>();
+        File file = new File("listAdmin.txt");
         if (!file.exists()) {
             try {
                 file.createNewFile();
@@ -54,12 +52,17 @@ private FileManagerBill() {}
         try {
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            billList = (List<Bill>) ois.readObject();
+            adminList = (List<Admin>) ois.readObject();
             ois.close();
             fis.close();
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return billList;
+        return adminList;
+
     }
 }
