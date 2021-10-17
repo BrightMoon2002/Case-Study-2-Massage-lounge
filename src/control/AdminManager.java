93dc16c91d17f9d86b9fd12509e3fed0d44d7473
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-10-17 17:15:45 +0700

    Add getter and setter in Admin manager

--- src/control/AdminManager.java
+++ src/control/AdminManager.java
@@ -1,4 +1,48 @@
 package control;
 
-public class AdminManager {
+import login.Admin;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AdminManager implements IGeneralManager<Admin> {
+    private List<Admin> adminList;
+    private
+
+    @Override
+    public List<Admin> findAll() {
+        return null;
+    }
+
+    @Override
+    public void saveList(Admin admin) throws IOException {
+
+    }
+
+    @Override
+    public void removeByIndex(int index) throws IOException {
+
+    }
+
+    @Override
+    public void updateByIndex(int index, Admin admin) throws IOException {
+
+    }
+
+    @Override
+    public Admin searchById(String id) {
+        return null;
+    }
+
+    @Override
+    public Admin searchByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void showAllList() {
+
+    }
+
+
 }
