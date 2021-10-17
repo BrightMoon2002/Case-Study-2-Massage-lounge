db0136b34223a4aeaea1dac39ff1c06f8a878fc4
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-10-17 16:24:28 +0700

    Add write and read file class idol

--- src/storage/FileManagerIdol.java
+++ src/storage/FileManagerIdol.java
@@ -1,10 +1,65 @@
 package storage;
 
+import model.Customer;
 import model.idol.Idol;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileManagerIdol {
-    List<Idol> idolList = new ArrayList<>();
+public class FileManagerIdol extends BaseFileManager<Idol>{
+    private static FileManagerIdol fileManagerIdol;
+    private FileManagerIdol() {}
+    public static FileManagerIdol getInstance() {
+        if (fileManagerIdol == null) {
+            fileManagerIdol = new FileManagerIdol();
+        }
+        return fileManagerIdol;
+    }
+
+    @Override
+    void writeList(List<Idol> list) {
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        File file = new File("listIdol.txt");
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(list);
+            oos.close();
+            fos.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    List<Idol> readList() {
+        List<Idol> idolList = new ArrayList<>();
+        File file = new File("listIdol.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (file.length() == 0) {
+            return new ArrayList<>();
+        }
+        try {
+            FileInputStream is = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            idolList = (List<Idol>) ois.readObject();
+            ois.close();
+            is.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return idolList;
+    }
 }
